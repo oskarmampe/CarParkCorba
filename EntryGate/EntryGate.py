2b746e1029152126c85f330dec38e44e471c6978
Oskar Mampe <oskarmampe@hotmail.co.uk>
2019-03-12 23:16:35 +0000

    Added basic Entry Gate functionality

--- EntryGate/EntryGate.py
+++ EntryGate/EntryGate.py
@@ -6,38 +6,28 @@ import Server__POA
 
 
 class EntryGate(Server__POA.EntryGate):
-    def __init__(self, machine_name, argv):
-        self.machine_name = machine_name
+    def __init__(self, argv, localServer):
+        self.machine_name = ""
         self.argv = argv
+        self.localServer = localServer
 
-    def turn_on(self):
+
+    def turn_on(self, new_machine_name):
         try:
-            # Initialize the ORB
-            orb = CORBA.ORB_init(self.argv, CORBA.ORB_ID)
-
-            # Get a reference to the Naming service
-            ns = orb.resolve_initial_references("NameService");
-            rootContext = ns._narrow(CosNaming.NamingContext)
-
-            if rootContext is None:
-                print "Failed to narrow the root naming context"
-                sys.exit(1)
-
-            # resolve the Count object in the Naming service
-            localServerName = [CosNaming.NameComponent("localServerName", "")]
-
-            # Get the actual EntryGate Object
-            localServer = rootContext.resolve(localServerName)
             in_args = [CORBA.TC_string, CORBA.TC_string]
-            result = localServer._dynamic_op("add_entry_gate", in_args=in_args, out_args=None)
-            result("hello", "from python")
+            result = self.localServer._dynamic_op("add_entry_gate", in_args=in_args, out_args=None)
+            result(new_machine_name, ", ior from python")
 
-            print("Turned On")
-        except Exception as inst:
-            print(inst)
+            print "Turned On..."
+        except Exception as exc:
+            print "Could not turn on.\nError using Corba: {}\nExiting now....".format(exc)
 
     def turn_off(self):
-        pass
+        try:
+            print "Turned Off..."
+        except Exception as exc:
+            print "Could not turn on.\nError using Corba: {}\nExiting now....".format(exc)
 
-    def reset(self):
-        pass
+    def reset(self, new_machine_name):
+        self.turn_off()
+        self.turn_on(new_machine_name)
--- EntryGate/main.py
+++ EntryGate/main.py
@@ -1,15 +1,66 @@
-#Default
-
+#Entry Gate entry point
 import sys
+
+from omniORB import CORBA, PortableServer
+
+import CosNaming
 
 from EntryGate import EntryGate
 
 def main(argv):
-    try:
-        eg = EntryGate("machine", sys.argv)
-        eg.turn_on()
-    except Exception as inst:
-        print(inst)
+  try:
+    # Initialize the ORB
+    orb = CORBA.ORB_init(argv, CORBA.ORB_ID)
+
+    # Get a reference to the Naming service
+    ns = orb.resolve_initial_references("NameService");
+    rootContext = ns._narrow(CosNaming.NamingContext)
+
+    if rootContext is None:
+      print "Failed to narrow the root naming context"
+      sys.exit(1)
+
+    localServerName = [CosNaming.NameComponent("localServerName", "")]
+    localServer = rootContext.resolve(localServerName)
+  except Exception as exc:
+    print "Error initialising Corba: {}\n Exiting now....".format(exc)
+    exit()
+
+
+  eg = EntryGate(sys.argv, localServer)
+  eg.turn_on("machine")
+  turned_on = True
+  user_input = ""
+  while user_input.lower() != 'q':
+
+    if user_input.lower() == 'f' and not turned_on:
+      print "\nMachine already turned off.\n"
+
+    if user_input.lower() == 'n' and turned_on:
+      print "\nMachine already turned on.\n"
+
+    if user_input.lower() == 'r' and not turned_on:
+      print "\nMachine must be turned on to reset."
+
+    if user_input.lower() == 'f' and turned_on:
+      eg.turn_off()
+      turned_on = False
+
+    if user_input.lower() == 'n' and not turned_on:
+      new_machine_name = raw_input("Type in machine name\n")
+      eg.turn_on(new_machine_name)
+      turned_on = True
+
+    if user_input.lower() == 'r' and turned_on:
+      new_machine_name = raw_input("Type in machine name\n")
+      eg.reset(new_machine_name)
+
+    user_input = raw_input("\nPress f to turn off, n to turn on, r to reset, or q to quit\n")
+
+    if user_input.lower() == 'q' and turned_on:
+      user_input = ""
+      print "\nPlease turn off the machine first."
+
 
 
 if __name__ == '__main__':
